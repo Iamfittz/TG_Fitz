4557983be08e72f7b3ef3ce43af8f1792ca87fde
Iamfitz <vuitsyk.a@gmail.com>
2025-04-02 22:52:36 +0300

    SRP concern

---
+++ Core/ICalculationResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TG_Fitz.Core
+{
+    public interface ICalculationResult
+    {
+        decimal TotalInterest { get; set; }
+        decimal TotalPayment { get; set; }
+    }
+}
