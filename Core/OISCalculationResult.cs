6b7f8cd690c73a6d056aa5a7db63c66e9fa7efd6
Iamfitz <vuitsyk.a@gmail.com>
2025-04-02 22:53:10 +0300

    Basic inheritance

--- Core/OISCalculationResult.cs
+++ Core/OISCalculationResult.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TG_Fitz.Core;
 
 namespace TelegramBot_Fitz.Core
 {
-    public class OISCalculationResult
+    public class OISCalculationResult :ICalculationResult
     {
         public decimal TotalInterest { get; set; }
         public decimal TotalPayment { get; set; }
