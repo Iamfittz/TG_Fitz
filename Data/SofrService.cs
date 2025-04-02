274ec09aebe81bf0e28e19418cf1419c91668f1f
Iamfitz <vuitsyk.a@gmail.com>
2025-04-02 22:54:13 +0300

    New Data logic and Database SOFR updates

--- Data/SofrService.cs
+++ Data/SofrService.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 using System.Text.Json;
+using Microsoft.EntityFrameworkCore;
 
 
 namespace TG_Fitz.Data
@@ -12,9 +13,11 @@ namespace TG_Fitz.Data
     public class SofrService
     {
         private readonly HttpClient _httpClient;
-        public SofrService(HttpClient httpClient)
+        private readonly AppDbContext _dbContext;
+        public SofrService(HttpClient httpClient,AppDbContext appDbContext)
         {
             _httpClient = httpClient;
+            _dbContext = appDbContext;
         }
         
         public async Task<string> GetLatestSofrAsync()
@@ -36,6 +39,25 @@ namespace TG_Fitz.Data
 
             string date = observation.GetProperty("date").GetString()!;
             string value = observation.GetProperty("value").GetString()!;
+
+            var dateValue = DateTime.Parse(date);
+            var decimalvalue = decimal.Parse(value);
+
+            var exists = _dbContext.InterestRates
+                .Any(r=>r.Date == dateValue && r.RateType=="SOFR");
+
+            if (!exists)
+            {
+                var newRate = new InterestRates
+                {
+                    Date = dateValue,
+                    InterestRateValue = decimalvalue,
+                    RateType = "SOFR"
+                };
+                _dbContext.InterestRates.Add(newRate);
+                await _dbContext.SaveChangesAsync();
+                Console.WriteLine("SOFR saved to database");
+            }
 
             return $"Sofr on {date}: {value}%";
         }
