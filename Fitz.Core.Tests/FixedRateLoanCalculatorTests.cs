216c6346c95979621af6e008effa698e9398d895
Iamfitz <vuitsyk.a@gmail.com>
2025-07-06 13:51:13 +0300

    Add Calculator Test

--- Fitz.Core.Tests/FixedRateLoanCalculatorTests.cs
+++ Fitz.Core.Tests/FixedRateLoanCalculatorTests.cs
@@ -2,23 +2,26 @@ using Fitz.Core.Enums;
 using TelegramBot_Fitz.Core;
 
 namespace Fitz.Core.Tests {
-    public class CalculateLoanTests {
+    public class FixedRateLoanCalculatorTests {
         [Fact]
         public void CalculateLoan_ShouldReturnCorrectTotalPayment() {
             // Arrange
             var calculator = new FixedRateLoanCalculator();
-            decimal loanAmount = 100_000m;
-            decimal[] yearlyRates = Enumerable.Repeat(6.0m, 15).ToArray(); 
+            decimal loanAmount = 1000m;
+            decimal[] yearlyRates = { 5, 10 }; // 5% in 1st year, 10 in second 
+            //1st 1000*5% = 50
+            //Second 1000 * 10% =100
+            // Total = 1000+50+100=1150
             var calculationType = InterestCalculationType.Simple;
-
             // Act
             var result = calculator.CalculateLoan(loanAmount, yearlyRates, calculationType);
-
             // Assert
-            decimal expectedInterest = loanAmount * 0.06m * 15; // простые проценты
-            decimal expectedTotalPayment = loanAmount + expectedInterest;
+            Assert.Equal(150, result.TotalInterest);
+            Assert.Equal(1150, result.TotalPayment);
 
-            Assert.Equal(expectedTotalPayment, result.TotalPayment);
+            Assert.Equal(2, result.YearlyCalculations.Length);
+            Assert.Equal(50, result.YearlyCalculations[0].Interest);
+            Assert.Equal(100, result.YearlyCalculations[1].Interest);
         }
 
     }
