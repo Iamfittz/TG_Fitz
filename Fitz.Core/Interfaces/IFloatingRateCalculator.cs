1bc86a5dac2fc224b27acd7b85e569b0a9535775
Iamfitz <vuitsyk.a@gmail.com>
2025-07-12 18:09:26 +0300

    Add IFloatingRateCalculator

---
+++ Fitz.Core/Interfaces/IFloatingRateCalculator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fitz.Core.Interfaces;
+using Fitz.Core.States;
+using Fitz.Core.Models;
+
+namespace Fitz.Core.Interfaces {
+    public interface IFloatingRateCalculator : ILoanCalculator {
+        List<PeriodInterestDetail> GetInterestBreakdown(UserState state);
+        decimal CalculateTotalInterest(UserState state);
+    }
+}
