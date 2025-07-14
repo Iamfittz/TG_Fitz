4ac1bf5d7a7838f9732e87fd6f732aa08090153c
Iamfitz <vuitsyk.a@gmail.com>
2025-07-14 10:37:27 +0300

    Create enum DayCountConvention

---
+++ Fitz.Core/Enums/DayCountConvention.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fitz.Core.Enums {
+    public enum DayCountConvention {
+        Actual360,
+        Actual365,
+        Thirty360,
+        ActualActual
+    }
+}
