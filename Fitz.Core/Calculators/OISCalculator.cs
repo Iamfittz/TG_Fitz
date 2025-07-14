ae866c4aa90cff6680c34426ff41b584ca6b6d2c
Iamfitz <vuitsyk.a@gmail.com>
2025-07-14 10:37:14 +0300

    Add conventions to OISCalculator

--- Fitz.Core/Calculators/OISCalculator.cs
+++ Fitz.Core/Calculators/OISCalculator.cs
@@ -6,29 +6,43 @@ using System.Threading.Tasks;
 using Fitz.Core.States;
 using Fitz.Core.Interfaces;
 using Fitz.Core.Models;
+using Fitz.Core.Enums;
+using Microsoft.VisualBasic;
 
 namespace TelegramBot_Fitz.Core
 {
     public class OISCalculator : ILoanCalculator
     {
-        //public decimal CalculateInterest(UserState state)
-        //{
-        //    return CalculateInterest(state.LoanAmount, state.FirstRate, state.Days);
-        //}
-        private decimal CalculateInterest(decimal amount, decimal rate, int days)
+        private decimal CalculateInterest(decimal amount, decimal rate, int days, DayCountConvention dayCountConvention)
         {
-            decimal dailyRate = rate / 360;
+            decimal denominator = dayCountConvention switch {
+                DayCountConvention.Actual360 => 360,
+                DayCountConvention.Actual365 =>365,
+                DayCountConvention.Thirty360 =>360,
+                DayCountConvention.ActualActual =>DateTime.IsLeapYear(DateTime.Now.Year) ? 366 : 365,
+                _ =>360
+            };
+
+            decimal dailyRate = rate / denominator;
             return amount * (dailyRate / 100) * days;
         }
-        public decimal CalculateTotalPayment(UserState state)
+        public decimal CalculateTotalPayment(UserState state, DayCountConvention dayCountConvention)
         {
-            return state.LoanAmount + CalculateInterest(state.LoanAmount, state.FirstRate, state.Days);
+            return state.LoanAmount + CalculateInterest(state.LoanAmount, state.FirstRate, state.Days, dayCountConvention);
         }
-        public OISCalculationResult CalculateOIS(UserState state)
+        public OISCalculationResult CalculateOIS(UserState state, DayCountConvention dayCountConvention)
         {
-            decimal dailyRate = state.FirstRate / 360;
-            decimal totalInterest = CalculateInterest(state.LoanAmount, state.FirstRate, state.Days);
-            decimal totalPayment = CalculateTotalPayment(state);
+            decimal denominator = dayCountConvention switch {
+                DayCountConvention.Actual360 => 360,
+                DayCountConvention.Actual365 => 365,
+                DayCountConvention.Thirty360 => 360,
+                DayCountConvention.ActualActual => DateTime.IsLeapYear(DateTime.Now.Year) ? 366 : 365,
+                _ => 360
+            };
+
+            decimal dailyRate = state.FirstRate / denominator;
+            decimal totalInterest = CalculateInterest(state.LoanAmount, state.FirstRate, state.Days, dayCountConvention);
+            decimal totalPayment = CalculateTotalPayment(state, dayCountConvention);
             return new OISCalculationResult
             {
                 DailyRate = dailyRate,
